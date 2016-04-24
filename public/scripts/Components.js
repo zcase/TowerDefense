b8cd79cd2d4b7400954e962e5261ac061ba10911
Nick Hunsaker <nickhunsaker@NIcks-MacBook-Air.local>
2016-04-23 20:20:16 -0600

    Add new waves

--- public/scripts/Components.js
+++ public/scripts/Components.js
@@ -1267,6 +1267,9 @@ towerDefense.components = (function(graphics, sound, effects) {
 				base.update(elapsedTime, false);
 			}
 
+            if(that.initialDelayTime >= that.startTime && xGrid < 0 ) {
+             that.moveForward(elapsedTime);
+         }
 			
             
 			didMoveForward = false;
